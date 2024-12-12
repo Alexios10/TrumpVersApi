084ef836f878e843bfa0b023243a849119a66e1f
Nadeem Ahmed <nadahmed03@hotmail.com>
2024-12-12 21:18:54 +0100

    Lagt til kommentarer i interfaces og Context

--- Interfaces/IStaffMembers.cs
+++ Interfaces/IStaffMembers.cs
@@ -1,12 +1,14 @@
-namespace TrumpVersApi.interfaces;
-
-
-interface IStaffmembers
+namespace TrumpVersApi.Interfaces
 {
-    int Id { get; set; }
-    string? Name { get; set; }
-    string? Image { get; set; }
-    string? Description { get; set; }
-    string? Title { get; set; }
-    string? Email { get; set; }
+    // Definerer et grensesnitt for StaffMembers
+    interface IStaffmembers
+    {
+        // Egenskap til å representere de ulike identifikatorene til ansatte
+        int Id { get; set; }
+        string? Name { get; set; }
+        string? Image { get; set; }
+        string? Description { get; set; }
+        string? Title { get; set; }
+        string? Email { get; set; }
+    }
 }
